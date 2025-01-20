3f30c81d29be9b2658d012278306aecdc8fbbc82
André Cunha <151823216+andre-mcunha@users.noreply.github.com>
2025-01-20 10:33:34 +0000

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+import React from 'react'
 import { Button } from '@headlessui/react'
 
-export default function Example() {
+export default function Page() {
   return (
     <Button className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[hover]:data-[active]:bg-sky-700">
       Save changes
